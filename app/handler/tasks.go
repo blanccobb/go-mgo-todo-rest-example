36b48db848445600a3a1ffb06897c696efc7e158
blanccobb <121212112@naver.com>
2019-02-20 12:29:39 +0900

    handler/ modified

--- app/handler/tasks.go
+++ app/handler/tasks.go
@@ -11,6 +11,7 @@ import (
 	"github.com/blanccobb/go-mgo-todo-rest-example/config"
 )
 
+
 func GetAllTasks(db *mgo.Database, w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	
--- app/handler/todo.go
+++ app/handler/todo.go
@@ -11,6 +11,7 @@ import (
 	"github.com/blanccobb/go-mgo-todo-rest-example/config"
 )
 
+
 func GetAllTodo(db *mgo.Database, w http.ResponseWriter, r *http.Request) {
 	todo := []model.Todo{}
 	err := db.C(config.COLLECTION).Find(bson.M{}).All(&todo)
