e1bd729ebc3dcffff585ce59288127ef4f75e3d6
blanccobb <121212112@naver.com>
2019-02-20 12:57:56 +0900

    add app

--- app/app.go
+++ app/app.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/globalsign/mgo"
 	"github.com/blanccobb/go-mgo-todo-rest-example/config"
+	"github.com/blanccobb/go-mgo-todo-rest-example/app/handler"
 )
 
 type App struct {
@@ -30,19 +31,19 @@ func (app *App) Init() {
 func (app *App) setRouters() {
 	app.Get("/todo", app.GetAllTodo)
 	app.Post("/todo", app.CreateTodo)
-	app.Get("/todo/{title}", app.GetTodo)
-	app.Put("/todo/{title}", app.UpdateTodo)
-	app.Delete("/todo/{title}", app.DeleteTodo)
-	app.Put("/todo/{title}/archeive", app.ArcheiveTodo)
-	app.Delete("/todo/{title}/archeive", app.RestoreTodo)
+	app.Get("/todo/{id}", app.GetTodo)
+	app.Put("/todo/{id}", app.UpdateTodo)
+	app.Delete("/todo/{id}", app.DeleteTodo)
+	app.Put("/todo/{id}/archeive", app.ArcheiveTodo)
+	app.Delete("/todo/{id}/archeive", app.RestoreTodo)
 	
-	app.Get("/todo/{title}/tasks", app.GetAllTasks)
-	app.Post("/todo/{title}/tasks", app.CreateTask)
-	app.Get("/todo/{title}/tasks/{id:[0-9]+}", app.GetTask)
-	app.Put("/todo/{title}/tasks/{id:[0-9]+}", app.UpdateTask)
-	app.Delete("/todo/{title}/tasks/{id:[0-9]+}", app.DeleteTask)
-	app.Put("/todo/{title}/tasks/{id:[0-9]+}/complete", app.CompleteTask)
-	app.Delete("/todo/{title}/tasks/{id:[0-9]+}/complete", app.UndoTask)
+	app.Get("/todo/{id}/tasks", app.GetAllTasks)
+	app.Post("/todo/{id}/tasks", app.CreateTask)
+	app.Get("/todo/{id}/tasks/{title}", app.GetTask)
+	app.Put("/todo/{id}/tasks/{title}", app.UpdateTask)
+	app.Delete("/todo/{id}/tasks/{title}", app.DeleteTask)
+	app.Put("/todo/{id}/tasks/{title}/complete", app.CompleteTask)
+	app.Delete("/todo/{id}/tasks/{title}/complete", app.UndoTask)
 }
 
 func (app *App) Get(path string, f func(w http.ResponseWriter, r *http.Response)) {
@@ -65,62 +66,64 @@ func (app *App) Delete(path string, f func(w http.ResponseWriter, r *http.Respon
 // Todo Handler
 
 func (app *App) GetAllTodo(w http.ResponseWriter, r *http.Response) {
-	
+	handler.GetAllTodo(app.DB, w, r)	
 }
 
 func (app *App) CreateTodo(w http.ResponseWriter, r *http.Response) {
-	
+	handler.CreateTodo(app.DB, w, r)	
 }
 
 func (app *App) GetTodo(w http.ResponseWriter, r *http.Response) {
-	
+	handler.GetTodo(app.DB, w, r)
 }
 
 func (app *App) UpdateTodo(w http.ResponseWriter, r *http.Response) {
-	
+	handler.UpdateTodo(app.DB, w, r)
 }
 
 func (app *App) DeleteTodo(w http.ResponseWriter, r *http.Response) {
-	
+	handler.DeleteTodo(app.DB, w, r)
 }
 
 func (app *App) ArcheiveTodo(w http.ResponseWriter, r *http.Response) {
-	
+	handler.ArchiveTodo(app.DB, w, r)
 }
 
 func (app *App) RestoreTodo(w http.ResponseWriter, r *http.Response) {
-	
+	handler.RestoreTodo(app.DB, w, r)
 }
 
 // Task Handler
 
 func (app *App) GetAllTasks(w http.ResponseWriter, r *http.Response) {
-	
+	handler.GetAllTasks(app.DB, w, r)
 }
 
 func (app *App) CreateTask(w http.ResponseWriter, r *http.Response) {
-	
+	handler.CreateTasks(app.DB, w, r)
 }
 
 func (app *App) GetTask(w http.ResponseWriter, r *http.Response) {
-	
+	handler.GetTasks(app.DB, w, r)
 }
 
 func (app *App) UpdateTask(w http.ResponseWriter, r *http.Response) {
-	
+	handler.UpdateTasks(app.DB, w, r)
 }
 
 func (app *App) DeleteTask(w http.ResponseWriter, r *http.Response) {
-	
+	handler.DeleteTasks(app.DB, w, r)
 }
 
 func (app *App) CompleteTask(w http.ResponseWriter, r *http.Response) {
-	
+	handler.CompleteTasks(app.DB, w, r)
 }
 
 func (app *App) UndoTask(w http.ResponseWriter, r *http.Response) {
-	
+	handler.UndoTasks(app.DB, w, r)
 }
 
-
+func (app *App) Run(host string) {
+	log.Fatal(http.ListenAndServe(host, app.Router))
+}
 
--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"fmt"
 	
-	//"github.com/blanccobb/go-mgo-todo-rest-example/config"
+	"github.com/blanccobb/go-mgo-todo-rest-example/config"
+	"github.com/blanccobb/go-mgo-todo-rest-example/app"
 	
-	
-)
+)				
 
 func main() {
 	fmt.Println("Start Restful api for file information!")
 	
-	
-
 }
 
